c9a048bb7c95c8338c4a28877e7e9382b26dc8e0
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-06-12 20:40:52 +0300

    Added solution

---
+++ Js Fundamentals/01. Basic Syntax, Conditional Statements and Loops/1.2. Basic Syntax, Conditional Statements and Loops - Exercise/06. Print And Sum.js
@@ -0,0 +1,12 @@
+function printAndSum(start, end) {
+    let numbers = "";
+    let sum = 0;
+    
+    for (let i = start; i <= end; i++) {
+        numbers += i + " ";
+        sum += i;
+    }
+    
+    console.log(numbers);
+    console.log(`Sum: ${sum}`);
+}

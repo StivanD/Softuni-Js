61d228bc300fb66e6845545b8646083cc91e2f8b
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-06-13 00:53:02 +0300

    Added solution

---
+++ Js Fundamentals/02. Data Types and Variables/2.1. Data Types and Variables - Lab/10. Triples of Latin Letters.js
@@ -0,0 +1,12 @@
+function triplesOfLatinLetters(n) {
+    for (let i = 0; i < n; i++) {
+        for (let j = 0; j < n; j++) {
+            for (let k = 0; k < n; k++) {
+                let firstLetter = String.fromCharCode(97 + i);
+                let secondLetter = String.fromCharCode(97 + j);
+                let thirdLetter = String.fromCharCode(97 + k)
+                console.log(`${firstLetter}${secondLetter}${thirdLetter}`);
+            }
+        }
+    }
+}

9bde433b48b5dcbc33648c4ff60bd46dfcf30386
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-06-12 23:48:04 +0300

    Added solution

---
+++ Js Fundamentals/01. Basic Syntax, Conditional Statements and Loops/1.3. Basic Syntax, Conditional Statements and Loops - More Exercise/03. Next Day.js
@@ -0,0 +1,6 @@
+function nextDay(year, month, day) {
+    var date = new Date(year, month - 1, day);
+    var oneDay = 24 * 60 * 60 * 1000;
+    var nextDate = new Date(date.getTime() + oneDay);
+    console.log(`${nextDate.getFullYear()}-${(nextDate.getMonth() + 1)}-${nextDate.getDate()}`);
+}

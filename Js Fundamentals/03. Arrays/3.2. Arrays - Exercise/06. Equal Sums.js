2a19e3a8c694b27a30811a1cd996bf4c59769005
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-06-14 01:02:04 +0300

    Added solution

---
+++ Js Fundamentals/03. Arrays/3.2. Arrays - Exercise/06. Equal Sums.js
@@ -0,0 +1,31 @@
+function solve(arr) {
+    let leftSum = 0;
+    let rightSum = 0;
+    let index = 0;
+    let flag = false;
+
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            rightSum += arr[j]
+        }
+
+        for (let j = i - 1; j >= 0; j--) {
+            leftSum += arr[j];
+        }
+
+        if (rightSum === leftSum) {
+            flag = true;
+            index = i;
+            break;
+        }
+
+        leftSum = 0;
+        rightSum = 0;
+    }
+
+    if (flag) {
+        console.log(index);
+    } else {
+        console.log("no");
+    }
+}

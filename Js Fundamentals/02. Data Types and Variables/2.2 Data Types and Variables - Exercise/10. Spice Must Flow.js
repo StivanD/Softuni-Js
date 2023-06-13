44db6d62f18407497b23bacdf7dda8cd89306b6e
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-06-13 18:17:51 +0300

    Added solution

---
+++ Js Fundamentals/02. Data Types and Variables/2.2 Data Types and Variables - Exercise/10. Spice Must Flow.js
@@ -0,0 +1,19 @@
+function spiceMustFlow(startingYield) {
+    let spices = 0;
+    let days = 0;
+    
+    while (startingYield >= 100) {
+        days++;
+        spices += startingYield;
+        if (spices >= 26) {
+            spices -= 26;
+        }
+        startingYield -= 10;
+    }
+    
+    spices -=26
+    
+    console.log(days);
+    console.log(spices);
+}
+

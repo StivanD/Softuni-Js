531d687cf2189d3d647ab74c32c521b5a2e478c3
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-06-15 23:31:29 +0300

    Added solution

---
+++ Js Fundamentals/04. Functions/4.1. Functions - Lab/03. Repeat String.js
@@ -0,0 +1,9 @@
+function repeatString(text, count) {
+    let newString = '';
+    
+    for (let i = 0; i < count; i++) {
+        newString += text;
+    }
+    
+    return console.log(newString);
+}

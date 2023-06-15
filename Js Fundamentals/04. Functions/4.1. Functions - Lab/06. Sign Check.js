6f7ebe1e082f1dfbe848d24e2b9c401be295156f
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-06-16 00:01:17 +0300

    Added solution

--- Js Fundamentals/04. Functions/4.1. Functions - Lab/06. Sign Check.js
+++ Js Fundamentals/04. Functions/4.1. Functions - Lab/06. Sign Check.js
@@ -1,3 +1,29 @@
 function signCheck(a, b, c){
     return (a * b * c > 0) ? 'Positive' : 'Negative';
 }
+
+/*
+function wrongResult(numOne, numTwo, numThree) {
+    let result = '';
+    if (numOne == 0 || numTwo == 0 || numThree == 0) {
+        result = 'Positive';
+    } else if (numOne > 0 && numTwo > 0 && numThree > 0) {
+        result = 'Positive';
+    } else if (numOne > 0 && numTwo > 0 && numThree < 0) {
+        result = 'Negative';
+    } else if (numOne > 0 && numTwo < 0 && numThree > 0) {
+        result = 'Negative';
+    } else if (numOne > 0 && numTwo < 0 && numThree < 0) {
+        result = 'Positive';
+    } else if (numOne < 0 && numTwo > 0 && numThree > 0) {
+        result = 'Negative';
+    } else if (numOne < 0 && numTwo > 0 && numThree < 0) {
+        result = 'Positive';
+    } else if (numOne < 0 && numTwo < 0 && numThree > 0) {
+        result = 'Positive';
+    } else if (numOne < 0 && numTwo < 0 && numThree < 0) {
+        result = 'Negative';
+    }
+    console.log(result);
+}
+*/

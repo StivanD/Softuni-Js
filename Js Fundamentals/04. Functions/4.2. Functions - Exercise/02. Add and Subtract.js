79d80dabc44c0401ebd156bb912e64e578201d3f
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-06-19 00:00:50 +0300

    Added solution

---
+++ Js Fundamentals/04. Functions/4.2. Functions - Exercise/02. Add and Subtract.js
@@ -0,0 +1,15 @@
+function addAndSubstract(a,b,c) {
+    let sum = sumTwo(a,b);
+    let sub = substract(c);
+
+function sumTwo(a,b) {
+    return a + b;
+}
+
+function substract(c) {
+    return sum - c;
+}
+
+
+console.log(sub);
+}

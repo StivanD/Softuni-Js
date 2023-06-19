c995016d890d70ba58d50209fb820b39cf0ce043
Stivan Dimitrov <stivandimitrov27@gmail.com>
2023-06-19 13:05:47 +0300

    Added solution

---
+++ Js Fundamentals/04. Functions/4.3. Functions - More Exercise/02. Number Modification.js
@@ -0,0 +1,23 @@
+function numberModification(number) {
+    let num = number.toString();
+    let sum = 0;
+  
+    const appendNum = n => (num += n);
+  
+    while (sum / num.length <= 5) {
+  
+      for (let i = 0; i < num.length; i++) {
+        let n = Number(num[i]);
+        sum += n;
+      }
+  
+      if (sum / num.length <= 5) {
+        appendNum(9);
+        sum = 0;
+      }
+    }
+    
+    console.log(num);
+}
+
+numberModification(101)
